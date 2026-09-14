3a1732c19520b97a8e110537943733c332e97ac3
Ashraffff <Ashrafff@email.com>
2026-09-14 21:54:07 +0300

    added generic repo and unitofwork

---
+++ E_Commerce.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IGenericRepository<TEntity , TKey>where TEntity : BaseEntity<TKey>
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        //They do not talk to the database , they update locally
+
+        Task<TEntity?> GetByIdAsync(TKey id, CancellationToken ct = default);
+        Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken ct = default);
+
+
+    }
+}
---
+++ E_Commerce.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.Common;
+using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    internal class GenericRepository<TEntity, TKey>(StoreDbContext dbContext) : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _dbContext = dbContext;
+
+        public void Add(TEntity entity) => _dbContext.Set<TEntity>().Add(entity);
+
+
+        public async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken ct = default)
+             => await _dbContext.Set<TEntity>().ToListAsync(ct);
+
+        public async Task<TEntity?> GetByIdAsync(TKey id, CancellationToken ct = default)
+            =>await _dbContext.Set<TEntity>().FindAsync(id, ct);
+
+        public void Remove(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
+
+
+        public void Update(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+
+    }
+}
