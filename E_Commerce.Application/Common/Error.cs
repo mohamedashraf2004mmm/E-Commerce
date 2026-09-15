20972f9b6618d66a3054cd5c9f340e353e8b8afd
Ashraffff <Ashrafff@email.com>
2026-09-15 07:21:25 +0300

    added DTOs and result pattern

---
+++ E_Commerce.Application/Common/Error.cs
@@ -0,0 +1,51 @@
+﻿namespace E_Commerce.Application.Common
+{
+    public record Error(string code , string Description , ErrorType ErrorType = ErrorType.Failure)
+    {
+        public static Error Failure(string code = "General.Failure" , string description = "General failure has occured")
+        {
+            return new Error(code, description, ErrorType.Failure);
+        }
+
+        public static Error Validation(string code = "General.Validation", string description = "General Validation has occured")
+        {
+            return new Error(code, description, ErrorType.Validation);
+        }
+
+        public static Error NotFound(string code = "General.NotFound", string description = "Resource not found")
+        {
+            return new Error(code, description, ErrorType.NotFound);
+        }
+
+        public static Error Conflict(string code = "General.Conflict", string description = "General Conflict has occured")
+        {
+            return new Error(code, description, ErrorType.Conflict);
+        }
+
+        public static Error Unauthorized(string code = "General.Unauthorized", string description = "UnAuthorized!!!")
+        {
+            return new Error(code, description, ErrorType.Unauthorized);
+        }
+
+        public static Error Forbidden(string code = "General.Forbidden", string description = "Forbidden!!!")
+        {
+            return new Error(code, description, ErrorType.Forbidden);
+        }
+
+        public static Error InvalidCredentials(string code = "General.InvalidCredentials", string description = "InvalidCredentials!!!")
+        {
+            return new Error(code, description, ErrorType.InvalidCredentials);
+        }
+    }
+
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validation = 1 ,
+        NotFound = 2,
+        Conflict = 3,
+        Unauthorized = 4,
+        Forbidden = 5,
+        InvalidCredentials = 6
+    }
+}
---
+++ E_Commerce.Application/Common/Result.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace E_Commerce.Application.Common
+{
+    public class Result
+    {
+        public bool IsSuccess { get;}
+        public IReadOnlyList<Error> Errors  { get; }
+
+        protected Result(bool isSuccess , IReadOnlyList<Error> errors)
+        {
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+
+        public static Result Ok() => new(true, Array.Empty<Error>());
+        public static Result Fail(Error error) => new(false, new[] { error });
+        public static Result Fail(IReadOnlyList<Error>errors) => new(false, errors);
+    }
+
+    public class Result<TValue> : Result
+    {
+        private readonly TValue value;
+
+        public TValue data => (IsSuccess) ? value : throw new InvalidOperationException("Can not access value of failed result");
+        private Result(TValue value) : base(true , Array.Empty<Error>())
+        {
+            this.value = value;
+        }
+
+        private Result(Error error) : base(false , new[] {error})
+        {
+            value = default!;
+        }
+
+        private Result(IReadOnlyList<Error> errors) : base(false, errors)
+        {
+            value = default!;
+        }
+
+        public static Result<TValue> Ok(TValue value) => new Result<TValue>(value);
+        public static Result Fail<TValue>(Error error) => new Result<TValue>(error);
+        public static Result Fail(IReadOnlyList<Error> errors) => new Result<TValue>(errors);
+    }
+}
